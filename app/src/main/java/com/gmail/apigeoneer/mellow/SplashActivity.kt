83d7b23ede2916d6e2fa4662b299f78498612223
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-09-01 14:05:22 +0530

    feat: splash screen

---
+++ app/src/main/java/com/gmail/apigeoneer/mellow/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.gmail.apigeoneer.mellow
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Hide status bar
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        // Lunch the Main Activity after some delay
+        // Deprecated
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+            },
+            2500
+        )
+    }
+}
