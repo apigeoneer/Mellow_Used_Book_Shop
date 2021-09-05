d3cd6c19b167063612c70a317aa3fe76f8fb7624
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-09-05 16:20:00 +0530

    add: login activity

---
+++ app/src/main/java/com/gmail/apigeoneer/mellow/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.gmail.apigeoneer.mellow.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gmail.apigeoneer.mellow.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
