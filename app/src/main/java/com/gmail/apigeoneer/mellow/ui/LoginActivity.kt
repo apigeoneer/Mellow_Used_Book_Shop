e3c1da3834784d390a4f18ab89adb3b17e06d93b
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-09-05 19:06:00 +0530

    add: login & register intents & data binding

--- app/src/main/java/com/gmail/apigeoneer/mellow/ui/LoginActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/mellow/ui/LoginActivity.kt
@@ -1,16 +1,22 @@
 package com.gmail.apigeoneer.mellow.ui
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
 import com.gmail.apigeoneer.mellow.R
+import com.gmail.apigeoneer.mellow.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         // Hide status bar
         @Suppress("DEPRECATION")
@@ -21,6 +27,17 @@ class LoginActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
+        }
+
+        navigateToRegisterScreen()
+    }
+
+    /**
+     * Navigate to the Register Activity
+     */
+    private fun navigateToRegisterScreen() {
+        binding.register.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/gmail/apigeoneer/mellow/ui/RegisterActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/mellow/ui/RegisterActivity.kt
@@ -1,12 +1,43 @@
 package com.gmail.apigeoneer.mellow.ui
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
 import com.gmail.apigeoneer.mellow.R
+import com.gmail.apigeoneer.mellow.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+
+        // Hide status bar
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        navigateToLoginScreen()
+    }
+
+    /**
+     * Navigate to the Login Activity
+     */
+    private fun navigateToLoginScreen() {
+        binding.login.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
